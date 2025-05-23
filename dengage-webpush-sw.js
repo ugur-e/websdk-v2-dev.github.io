10e3e5c4520e77dbac952144d661a904d694b092
ugur-e <109581858+ugur-e@users.noreply.github.com>
2025-05-23 16:35:49 +0300

    Update dengage-webpush-sw.js

--- dengage-webpush-sw.js
+++ dengage-webpush-sw.js
@@ -3,6 +3,6 @@ var swUrl = new URL(location);
 var accountId = swUrl.searchParams.get('account_id') || '';
 var appGuid = swUrl.searchParams.get('app_guid') || '';
 if (accountId && appGuid) {
-  importScripts("https://dev-pub.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
+  importScripts("https://dev.lib.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
 }
       
