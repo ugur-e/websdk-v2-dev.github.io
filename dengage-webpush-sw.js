d7ae22e887ab98a41c0114357864ac1b5df7b889
ugur-e <109581858+ugur-e@users.noreply.github.com>
2025-12-25 18:29:44 +0300

    Update dengage-webpush-sw.js
    version added to import sw file url

--- dengage-webpush-sw.js
+++ dengage-webpush-sw.js
@@ -1,8 +1,7 @@
-
 var swUrl = new URL(location);
 var accountId = swUrl.searchParams.get('account_id') || '';
 var appGuid = swUrl.searchParams.get('app_guid') || '';
+var version = swUrl.searchParams.get('version') || '';
 if (accountId && appGuid) {
-  importScripts("https://dev.lib.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js");
+  importScripts("https://dev.lib.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js?v=" + version);
 }
-      
