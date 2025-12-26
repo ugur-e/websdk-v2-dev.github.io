ad259f909e11b00e501d9765edd7744d6e3c656c
ugur-e <109581858+ugur-e@users.noreply.github.com>
2025-12-26 17:18:21 +0300

    Update dengage-webpush-sw.js
    hash added to sw url

--- dengage-webpush-sw.js
+++ dengage-webpush-sw.js
@@ -2,6 +2,7 @@ var swUrl = new URL(location);
 var accountId = swUrl.searchParams.get('account_id') || '';
 var appGuid = swUrl.searchParams.get('app_guid') || '';
 var version = swUrl.searchParams.get('version') || '';
+var hash = swUrl.searchParams.get('hash') || '';
 if (accountId && appGuid) {
-  importScripts("https://dev.lib.dengage.com/p/push/" + accountId + "/" + appGuid + "/dengage_sw.js?v=" + version);
+  importScripts("https://dev.lib.dengage.com/p/push/" + accountId + "/" + appGuid + "/sdk/" + version + "/dengage_sw."+ hash +".js");
 }
